28a357baf70ddc8027447c9d670a554127e48bb6
luna101 <abyssal512@gmail.com>
2018-10-08 11:49:17 +1300

    Document Device endpoints

--- Pusharp/Client/Devices.cs
+++ Pusharp/Client/Devices.cs
@@ -11,27 +11,54 @@ namespace Pusharp
 {
     public partial class PushBulletClient
     {
+        /// <summary>
+        ///     Retrieves a list of all devices under the client's account.
+        /// </summary>
+        /// <returns>A <see cref="Task"/> representing the asynchronous get operation. This task will resolve with a list of <see cref="Device"/> entities, representing the devices under the client's account.</returns>
         public async Task<IReadOnlyCollection<Device>> GetDevicesAsync()
         {
             var devicesModel = await _requests.SendAsync<DevicesModel>("/v2/devices", HttpMethod.Get, true, 1, null).ConfigureAwait(false);
             return devicesModel.Models.ToDevices().ToList();
         }
 
+        /// <summary>
+        ///     Creates a device under the client's account.
+        /// </summary>
+        /// <param name="parameters">The <see cref="DeviceParameters"/> to set up the new device with.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous post operation. This task will resolve with a <see cref="Device"/> entity representing the created device.</returns>
         public async Task<Device> CreateDeviceAsync(DeviceParameters parameters)
         {
             var deviceModel = await _requests.SendAsync<DeviceModel>("/v2/devices", HttpMethod.Post, true, 1, parameters).ConfigureAwait(false);
             return new Device(deviceModel);
         }
 
+        /// <summary>
+        ///     Updates a device's properties.
+        /// </summary>
+        /// <param name="device">The <see cref="Device"/> to update.</param>
+        /// <param name="parameters">The <see cref="DeviceParameters"/> to update the device with.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous post operation. This task will resolve with a <see cref="Device"/> entity representing the updated device.</returns>
         public Task<Device> UpdateDeviceAsync(Device device, DeviceParameters parameters)
             => UpdateDeviceAsync(device.Identifier, parameters);
 
+        /// <summary>
+        ///     Updates a device's properties.
+        /// </summary>
+        /// <param name="identifier">The string identifier of the device to update.</param>
+        /// <param name="parameters">The <see cref="DeviceParameters"/> to update the device with.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous post operation. This task will resolve with a <see cref="Device"/> entity representing the updated device.</returns>
         public async Task<Device> UpdateDeviceAsync(string identifier, DeviceParameters parameters)
         {
             var deviceModel = await _requests.SendAsync<DeviceModel>($"/v2/devices/{identifier}", HttpMethod.Post, true, 1, parameters).ConfigureAwait(false);
             return new Device(deviceModel);
         }
 
+        /// <summary>
+        ///     Deletes a device from the client's account.
+        /// </summary>
+        /// <param name="device">The <see cref="Device"/> to delete.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous delete operation.</returns>
+        /// <exception cref="Exception">Thrown if the device is not active.</exception>
         public Task DeleteDeviceAsync(Device device)
         {
             if(!device.IsActive)
@@ -40,6 +67,12 @@ namespace Pusharp
             return DeleteDeviceAsync(device.Identifier);
         }
 
+        /// <summary>
+        ///     Deletes a device from the client's account.
+        /// </summary>
+        /// <param name="identifier">The string identifier of the device to delete.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous delete operation.</returns>
+        /// <exception cref="Exception">Thrown if the device is not active.</exception>
         public async Task DeleteDeviceAsync(string identifier)
             => await _requests.SendAsync<EmptyModel>($"/v2/devices/{identifier}", HttpMethod.Delete, true, 1, null).ConfigureAwait(false);
     }
