f4cac3e9c93da64519c787553bfb9f18c4b74ab4
Casino Boyale <kayrenhi@gmail.com>
2018-10-07 23:57:32 +0100

    Added sending sms

---
+++ Pusharp/Client/Ephemerals.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Pusharp.Models;
+using Pusharp.RequestParameters;
+
+namespace Pusharp
+{
+    public partial class PushBulletClient
+    {
+        public async Task SendSmsAsync(SmsParameters parameters)
+            => await _requests.SendAsync<EmptyModel>("/v2/ephemerals", HttpMethod.Post, true, 1, parameters)
+                .ConfigureAwait(false);
+    }
+}
---
+++ Pusharp/RequestParameters/SmsParameters.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Voltaic.Serialization;
+using Voltaic.Serialization.Json;
+
+namespace Pusharp.RequestParameters
+{
+    public class SmsParameters : BaseRequest
+    {
+        [ModelProperty("type")]
+        public string Type { get; set; } = "push";
+
+        [ModelProperty("push")]
+        public Push Push { get; set; }
+
+        public override bool VerifyParameters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string BuildContent(JsonSerializer serializer)
+            => serializer.WriteUtf16String(this);
+    }
+
+    public class Push
+    {
+        [ModelProperty("conversation_iden")]
+        public string ConversationIdentifier { get; set; }
+
+        [ModelProperty("message")]
+        public string Message { get; set; }
+
+        [ModelProperty("target_device_iden")]
+        public string DeviceIdentifier { get; set; }
+
+        [ModelProperty("type")]
+        public string Type { get; set; } = "messaging_extension_reply";
+
+        [ModelProperty("package_name")]
+        public string PackageName { get; set; } = "com.pushbullet.android";
+
+        [ModelProperty("source_user_identifier")]
+        public string UserIdentifier { get; set; }
+    }
+}
