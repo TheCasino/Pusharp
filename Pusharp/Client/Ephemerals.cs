87d4dc6c0c7f6dcafe8f48e2a61b687121f0087b
luna101 <abyssal512@gmail.com>
2018-10-08 17:07:20 +1300

    Add Action<SmsParameter> overload to SMS sending

--- Pusharp/Client/Ephemerals.cs
+++ Pusharp/Client/Ephemerals.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Pusharp.Models;
 using Pusharp.RequestParameters;
@@ -15,5 +16,17 @@ namespace Pusharp
         public async Task SendSmsAsync(SmsParameters parameters)
             => await _requests.SendAsync<EmptyModel>("/ephemerals", HttpMethod.Post, true, 1, parameters)
                 .ConfigureAwait(false);
+
+        /// <summary>
+        ///     Sends an SMS message to a device.
+        /// </summary>
+        /// <param name="parameterOperator">The <see cref="Action{SmsParameters}"/> that configures the parameters to use when sending.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous send operation.</returns>
+        public Task SendSmsAsync(Action<SmsParameters> parameterOperator)
+        {
+            var parameters = new SmsParameters();
+            parameterOperator(parameters);
+            return SendSmsAsync(parameters);
+        }
     }
 }
