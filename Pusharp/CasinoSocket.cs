77e4812bc391ef908280e3a62d8b8db641125c43
Casino Boyale <kayrenhi@gmail.com>
2018-10-14 16:14:44 +0100

    Implemented my own websocket

---
+++ Pusharp/CasinoSocket.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pusharp
+{
+    internal class CasinoSocket
+    {
+        private readonly ClientWebSocket _socket;
+        private readonly UTF8Encoding _encoder;
+
+        private readonly ConcurrentQueue<IEnumerable<byte>> _bufferQueue;
+
+        public CasinoSocket()
+        {
+            _socket = new ClientWebSocket();
+            _encoder = new UTF8Encoding();
+
+            _bufferQueue = new ConcurrentQueue<IEnumerable<byte>>();
+        }
+
+        public async Task ConnectAsync(string url)
+        {
+            var uri = new Uri(url);
+            await _socket.ConnectAsync(uri, CancellationToken.None);
+
+            Task.Run(async () => await Receiving());
+        }
+
+        public event Func<string, Task> MessageReceived;
+
+        private Task InternalMessageReceived(string message)
+        {
+            return MessageReceived is null ? Task.CompletedTask : MessageReceived.Invoke(message);
+        }
+
+        private async Task Receiving()
+        {
+            while (_socket.State == WebSocketState.Open)
+            {
+                var buffer = new byte[1024];
+                var result = await _socket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                if (result.MessageType == WebSocketMessageType.Text)
+                {
+                    var trimmedBuffer = buffer.Take(result.Count);
+                    _bufferQueue.Enqueue(trimmedBuffer);
+
+                    if(result.EndOfMessage)
+                    {
+                        var builder = new StringBuilder();
+
+                        while (_bufferQueue.TryDequeue(out var qBuffer))
+                        {
+                            builder.Append(_encoder.GetString(qBuffer.ToArray()));
+                        }
+
+                        await InternalMessageReceived(builder.ToString());
+                    }
+                }
+
+                await Task.Delay(100);
+            }
+        }
+    }
+}
--- Pusharp/WebSocket.cs
+++ Pusharp/WebSocket.cs
@@ -1,10 +1,9 @@
-﻿using PureWebSockets;
-using Pusharp.Entities.WebSocket;
-using Pusharp.Models.WebSocket;
-using Pusharp.Utilities;
+﻿using Pusharp.Utilities;
 using System;
 using System.Net.WebSockets;
 using System.Threading.Tasks;
+using Pusharp.Entities.WebSocket;
+using Pusharp.Models.WebSocket;
 using Voltaic;
 using Voltaic.Serialization.Json;
 
@@ -15,67 +14,108 @@ namespace Pusharp
         private readonly PushBulletClient _client;
         private readonly PushBulletClientConfig _config;
         private readonly JsonSerializer _serializer;
-        private readonly Logger _logger;
 
-        private PureWebSocket _socket;
-
-        private const int RetryLimit = 5;
-        private int _attempts;
-
-        public WebSocket(PushBulletClient client, PushBulletClientConfig config, JsonSerializer serializer, Logger logger)
+        private readonly CasinoSocket _socket;
+        
+        public WebSocket(PushBulletClient client, PushBulletClientConfig config, JsonSerializer serializer)
         {
+            _socket = new CasinoSocket();
+            _socket.MessageReceived += MessageReceived;
+
             _client = client;
             _config = config;
             _serializer = serializer;
-            _logger = logger;
         }
 
-        public void Connect()
+        public async Task ConnectAsync()
         {
-            var options = new PureWebSocketOptions
-            {
-                SendDelay = 100,
-                IgnoreCertErrors = true
-            };
-
-            _socket = new PureWebSocket($"{_config.WebSocketUrl}/{_config.Token}", options);
-
-            _socket.OnStateChanged += StateChanged;
-            _socket.OnMessage += MessageReceived;
-            _socket.OnClosed += SocketClosed;
-            _socket.OnSendFailed += FailedSend;
-
             try
             {
-                _socket.Connect();
+                await _socket.ConnectAsync($"{_config.WebSocketUrl}{_config.Token}");
             }
             catch (AggregateException exception)
             {
-                _logger.InvokeLog(new LogMessage(LogLevel.Critical, exception.ToString()));
+                await _client.InternalLogAsync(new LogMessage(LogLevel.Critical, exception.ToString()));
             }
             catch (WebSocketException exception)
             {
-                _logger.InvokeLog(new LogMessage(LogLevel.Critical, exception.ToString()));
+                await _client.InternalLogAsync(new LogMessage(LogLevel.Critical, exception.ToString()));
             }
         }
 
+        private async Task MessageReceived(string socketMessage)
+        {
+            var model = _serializer.ReadUtf8<MessageReceivedModel>(new Utf8String(socketMessage));
+            var message = new WebSocketMessage(model);
+
+            switch (message.Type)
+            {
+                case MessageType.HEARTBEAT:
+                    await _client.InternalLogAsync(new LogMessage(LogLevel.Debug, "Heartbeat received"));
+                    break;
+                case MessageType.PUSH:
+                    await _client.InternalLogAsync(new LogMessage(LogLevel.Debug, "Push received"));
+
+                    switch (message.ReceivedModel.Type)
+                    {
+                        case "mirror":
+                            await _client.InternalLogAsync(new LogMessage(LogLevel.Debug, "Mirror push"));
+                            await _client.InternalPushReceivedAsync(new ReceivedPush(message.ReceivedModel));
+                            break;
+
+                        case "dismissal":
+                            await _client.InternalLogAsync(new LogMessage(LogLevel.Debug, "Dismissed push"));
+                            await _client.InternalPushDismissedAsync(new DismissedPush(message.ReceivedModel));
+                            break;
+
+                        case "clip":
+                            await _client.InternalLogAsync(new LogMessage(LogLevel.Debug, "Copy received"));
+                            await _client.InternalCopyReceivedAsync(new ReceivedCopy(message.ReceivedModel));
+                            break;
+                    }
+                    break;
+
+                case MessageType.TICKLE:
+                    await _client.InternalLogAsync(new LogMessage(LogLevel.Debug, "Tickle received"));
+
+                    switch (message.SubType)
+                    {
+                        case SubType.DEVICE:
+                            await _client.InternalLogAsync(new LogMessage(LogLevel.Debug, "Device updated"));
+                            await _client.UpdateDeviceCacheAsync();
+                            break;
+
+                        case SubType.PUSH:
+                            await _client.InternalLogAsync(new LogMessage(LogLevel.Debug, "Push updated"));
+                            await _client.UpdatePushCacheAsync();
+                            break;
+                    }
+
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(message.Type));
+            }
+        }
+
+        /*
         private void FailedSend(string data, Exception ex)
         {
-            _logger.InvokeLog(new LogMessage(LogLevel.Error, $"Failed to send: {ex}"));
+            await _client.InternalLogAsync(new LogMessage(LogLevel.Error, $"Failed to send: {ex}"));
         }
 
         private void SocketClosed(WebSocketCloseStatus reason)
         {
-            _logger.InvokeLog(new LogMessage(LogLevel.Error, $"Websocket closed: {reason}"));
+            await _client.InternalLogAsync(new LogMessage(LogLevel.Error, $"Websocket closed: {reason}"));
             _socket.Dispose(true);
 
             if (_attempts == RetryLimit)
             {
-                _logger.InvokeLog(new LogMessage(LogLevel.Critical, $"{RetryLimit} failed attemps to connect... Terminating connection"));
+                await _client.InternalLogAsync(new LogMessage(LogLevel.Critical, $"{RetryLimit} failed attemps to connect... Terminating connection"));
                 return;
             }
             
-            _logger.InvokeLog(new LogMessage(LogLevel.Info, "Trying to restart WebSocket..."));
+            await _client.InternalLogAsync(new LogMessage(LogLevel.Info, "Trying to restart WebSocket..."));
             _ = Task.Delay(TimeSpan.FromSeconds(5)).ContinueWith(_ =>
             {
                 _attempts++;
@@ -86,38 +126,56 @@ namespace Pusharp
         private void MessageReceived(string socketMessage)
         {
             var model = _serializer.ReadUtf8<MessageReceivedModel>(new Utf8String(socketMessage));
-
             var message = new WebSocketMessage(model);
 
+            Task.Run(() => MessageReceivedAsync(message));
+        }
+
+        private async Task MessageReceivedAsync(WebSocketMessage message)
+        {
             switch (message.Type)
             {
                 case MessageType.HEARTBEAT:
-                    _logger.InvokeLog(new LogMessage(LogLevel.Debug, "Heartbeat received"));
+                    await _client.InternalLogAsync(new LogMessage(LogLevel.Debug, "Heartbeat received"));
                     break;
                 case MessageType.PUSH:
-                    _logger.InvokeLog(new LogMessage(LogLevel.Debug, "Push received"));
+                    await _client.InternalLogAsync(new LogMessage(LogLevel.Debug, "Push received"));
 
                     switch (message.ReceivedModel.Type)
                     {
                         case "mirror":
-                            _logger.InvokeLog(new LogMessage(LogLevel.Debug, "Mirror push"));
+                            await _client.InternalLogAsync(new LogMessage(LogLevel.Debug, "Mirror push"));
                             _client.InternalPushReceivedAsync(new ReceivedPush(message.ReceivedModel));
                             break;
 
                         case "dismissal":
-                            _logger.InvokeLog(new LogMessage(LogLevel.Debug, "Dismissed push"));
+                            await _client.InternalLogAsync(new LogMessage(LogLevel.Debug, "Dismissed push"));
                             _client.InternalPushDismissedAsync(new DismissedPush(message.ReceivedModel));
                             break;
 
                         case "clip":
-                            _logger.InvokeLog(new LogMessage(LogLevel.Debug, "Copy received"));
+                            await _client.InternalLogAsync(new LogMessage(LogLevel.Debug, "Copy received"));
                             _client.InternalCopyReceivedAsync(new ReceivedCopy(message.ReceivedModel));
                             break;
                     }
                     break;
 
                 case MessageType.TICKLE:
-                    _logger.InvokeLog(new LogMessage(LogLevel.Debug, "Tickle received"));
+                    await _client.InternalLogAsync(new LogMessage(LogLevel.Debug, "Tickle received"));
+
+                    switch (message.SubType)
+                    {
+                        case SubType.DEVICE:
+                            await _client.InternalLogAsync(new LogMessage(LogLevel.Debug, "Device updated"));
+                            await _client.UpdateDeviceCacheAsync();
+                            break;
+
+                        case SubType.PUSH:
+                            await _client.InternalLogAsync(new LogMessage(LogLevel.Debug, "Push updated"));
+                            await _client.UpdatePushCacheAsync();
+                            break;
+                    }
+
                     break;
 
                 default:
@@ -129,9 +187,9 @@ namespace Pusharp
         {
             if (newState == WebSocketState.Open)
             {
-                _logger.InvokeLog(new LogMessage(LogLevel.Info, "WebSocket connection has been made"));
+                await _client.InternalLogAsync(new LogMessage(LogLevel.Info, "WebSocket connection has been made"));
                 _attempts = 0;
             }
-        }
+        }*/
     }
 }
