08d692471300f740cec6f5a7d1949bb6576b8bf8
Casino Boyale <kayrenhi@gmail.com>
2018-10-14 16:14:57 +0100

    Implemented caching

--- Pusharp/Endpoints/Client.cs
+++ Pusharp/Endpoints/Client.cs
@@ -1,9 +1,13 @@
 using Pusharp.Entities;
 using Pusharp.Models;
+using Pusharp.Utilities;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Data;
+using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
-using Pusharp.Utilities;
 using Voltaic.Serialization.Json;
 
 namespace Pusharp
@@ -15,13 +19,19 @@ namespace Pusharp
     {
         private readonly PushBulletClientConfig _config;
         private readonly JsonSerializer _serializer;
-        private readonly Logger _logger;
 
         internal readonly RequestClient RequestClient;
+
         /// <summary>
         ///     The current user that is logged into this client.
         /// </summary>
         public CurrentUser CurrentUser { get; private set; }
+
+        private readonly ConcurrentDictionary<string, Device> _devices;
+        public IReadOnlyCollection<Device> Devices => _devices.Values.Where(x => x.IsActive).ToImmutableList();
+
+        private readonly ConcurrentDictionary<string, Push> _pushes;
+        public IReadOnlyCollection<Push> Pushes => _pushes.Values.ToImmutableList();
 
         public PushBulletClient(PushBulletClientConfig config)
         {
@@ -30,9 +40,11 @@ namespace Pusharp
 
             _config = config;
             _serializer = new JsonSerializer();
-            _logger = new Logger(this, _config.LogLevel);
 
-            RequestClient = new RequestClient(this, _config, _serializer, _logger);
+            _devices = new ConcurrentDictionary<string, Device>();
+            _pushes = new ConcurrentDictionary<string, Push>();
+
+            RequestClient = new RequestClient(this, _config, _serializer);
         }
 
         public async Task ConnectAsync()
@@ -40,8 +52,55 @@ namespace Pusharp
             var authentication = await RequestClient.SendAsync<CurrentUserModel>("/v2/users/me", HttpMethod.Get, null).ConfigureAwait(false);
             CurrentUser = new CurrentUser(authentication, RequestClient);
 
-            var socket = new WebSocket(this, _config, _serializer, _logger);
-            socket.Connect();
+            var socket = new WebSocket(this, _config, _serializer);
+            await socket.ConnectAsync();
+
+            await InternalLogAsync(new LogMessage(LogLevel.Verbose, "Building cache"));
+            var devices = await GetDevicesAsync();
+
+            foreach (var device in devices)
+                _devices[device.Identifier] = device;
+
+            var pushes = await GetPushesAsync(null);
+
+            foreach (var push in pushes)
+                _pushes[push.Identifier] = push;
+
+            await InternalLogAsync(new LogMessage(LogLevel.Verbose, "Cache built"));
+            
+            await InternalReadyAsync();
+        }
+
+        internal async Task UpdateDeviceCacheAsync()
+        {
+            var ordered = Devices.OrderBy(x => x.Modified);
+            var recent = ordered.LastOrDefault();
+
+            if (recent is null)
+                return;
+
+            var deviceModels = await InternalGetDevicesAsync(recent.InternalModified);
+
+            foreach (var model in deviceModels)
+            {
+                _devices[model.Iden] = new Device(model, this);
+            }
+        }
+
+        internal async Task UpdatePushCacheAsync()
+        {
+            var ordered = Pushes.OrderBy(x => x.Modified);
+            var recent = ordered.LastOrDefault();
+
+            if (recent is null)
+                return;
+
+            var pushModels = await InternalGetPushesAsync(null, recent.InternalModified);
+
+            foreach (var model in pushModels)
+            {
+                _pushes[model.Identifier] = new Push(model, RequestClient);
+            }            
         }
     }
 }
